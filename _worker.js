ea2acac3428fd87b4370fc20f914f98fa3303e0b
hou42644 <165400844+hou42644@users.noreply.github.com>
2024-05-04 08:48:50 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '8fa51b49-da47-49b9-8399-a50a1cf18bc0';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
